3cd9ff435ae067d996d5941d1eacf6643c1d93bb
Heiko Schmidt <github@heikoschmidt.info>
2024-06-14 22:59:28 +0200

    add right sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
 
-    const loggedIn = { firstName: 'Joe' };
+    const loggedIn = { firstName: 'John', lastName: 'Doe', email: 'jdoe@example.com' };
 
     return (
     <section className='home'>
@@ -22,7 +23,15 @@ const Home = () => {
                     totalCurrentBalance={1250.35}
                 />
             </header>
+            
+            RECENT TRANSACTIONS
         </div>
+        
+        <RightSidebar 
+            user={loggedIn}
+            transactions={[]}
+            banks={[{currentBalance: 123.50}, { currentBalance: 500.0}]}
+        />
       </section>
     )
 }
