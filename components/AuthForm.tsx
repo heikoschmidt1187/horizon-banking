de9152496d2dd8bd1be1d97cca8c5f8a93f3867e
Heiko Schmidt <github@heikoschmidt.info>
2024-06-15 17:58:24 +0200

    Connect Plaid

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -12,6 +12,7 @@ import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { signIn, signUp } from '@/lib/actions/user.actions';
+import PlaidLink from './PlaidLink';
 
 const AuthForm = ({type}: {type: string}) => {
   const router = useRouter();
@@ -34,9 +35,21 @@ const AuthForm = ({type}: {type: string}) => {
     
     try {
       // Sign up with Appwrite & create plaid link token
+      const userData = {
+        firstName: data.firstName!,
+        lastName: data.lastName!,
+        address1: data.address1!,
+        city: data.city!,
+        state: data.state!,
+        postalCode: data.postalCode!,
+        dateOfBirth: data.dateOfBirth!,
+        ssn: data.ssn!,
+        email: data.email,
+        password: data.password
+      }
        
       if (type === 'sign-up') {
-          const newUser = await signUp(data);
+          const newUser = await signUp(userData);
           setUser(newUser);
       }
       
@@ -87,7 +100,7 @@ const AuthForm = ({type}: {type: string}) => {
       </header>
       {user ? (
         <div className='flex flex-col gap-4'>
-        { /* PlaidLink */ }
+          <PlaidLink user={user} variant='primary' />
         </div>
       ): (
         <>
